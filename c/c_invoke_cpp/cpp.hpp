cc32ae93dbdd91c40c42f78daca45a115a16a64c
hackin zhao <hackin.zhao@gmail.com>
2021-08-25 22:06:11 +0800

    first commit
    1. add c invoke cpp examples
    
    Signed-off-by: hackin zhao <hackin.zhao@gmail.com>

---
+++ c/c_invoke_cpp/cpp.hpp
@@ -0,0 +1,72 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2021 hackin zhao
+
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+
+#if !defined(_CPP_HPP_)
+#define _CPP_HPP_
+
+// namespace c_invoke_cpp {
+
+// class Cpp {
+// private:
+//     /* data */
+// public:
+//     Cpp(/* args */);
+//     ~Cpp();
+
+//     int fun(int i);
+// };
+
+// extern "C" double call_C_f(Cpp* p, int i) // wrapper function
+// {
+//     return p->fun(i);
+// }
+
+// } // namespace c_invoke_cpp
+
+#ifdef __cplusplus
+
+class CppExample {
+private:
+    /* data */
+public:
+    CppExample(/* args */);
+    ~CppExample();
+
+    int fun(int i);
+};
+
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double call_cpp_example_function(CppExample* p, int i) // wrapper function
+{
+    return p->fun(i);
+}
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // _CPP_HPP_
