d66e1209f0cfc4d7da9564c313a250cf50f99329
hackin zhao <hackin.zhao@stratosphere.mobi>
2021-09-18 19:27:29 +0800

    cpp: tls: update code
    Signed-off-by: hackin zhao <hackin.zhao@stratosphere.mobi>

--- cpp/tls/main.cpp
+++ cpp/tls/main.cpp
@@ -8,25 +8,26 @@
 #include <vector>
 #include <chrono>
 #include <random>
-using namespace std;
-using namespace chrono;
 
-thread_local default_random_engine dre;
+thread_local std::default_random_engine dre;
 
-vector<int> test_dre(int seed)
+std::vector<int> test_dre(int seed)
 {
     dre.seed(seed);
     int vecSize = 10;
-    vector<int> temp(vecSize);
+    std::vector<int> temp(vecSize);
     for (int i = 0; i < vecSize; ++i)
     {
         temp[i] = dre();
-        this_thread::sleep_for(milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+
+    std::cout << __FUNCTION__ << " thread id: " << std::this_thread::get_id() << std::endl;
+
     return temp;
 }
 
-bool equal(const vector<int> &v1, const vector<int> &v2)
+bool equal(const std::vector<int> &v1, const std::vector<int> &v2)
 {
     int size1 = v1.size();
     int size2 = v2.size();
@@ -44,17 +45,17 @@ bool equal(const vector<int> &v1, const vector<int> &v2)
 int main()
 {
     int numThread = 2;
-    using returnType = vector<int>;
 
-    future<returnType> future1 = async(test_dre, 0);
-    returnType result2 = test_dre(0);
-    returnType result1 = future1.get();
+    std::cout << __FUNCTION__ << " thread id: " << std::this_thread::get_id() << std::endl;
+
+    std::future<std::vector<int>> future1 = std::async(test_dre, 0);
+    std::vector<int> result2 = test_dre(0);
+    std::vector<int> result1 = future1.get();
 
     if (equal(result1, result2))
-        cout << "they are equal" << endl;
+        std::cout << "they are equal" << std::endl;
     else
-        cout << "they are not equal" << endl;
+        std::cout << "they are not equal" << std::endl;
 
-    system("pause");
     return 0;
 }
